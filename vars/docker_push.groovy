5b141039650d02f70526e8c1a56c2c64c31e888b
falgun14 <70535283+falgun14@users.noreply.github.com>
2025-04-15 02:18:23 +0530

    sudo

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "sudo docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
-  sh "sudo docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  sh " docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
